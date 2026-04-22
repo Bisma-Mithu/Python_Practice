60860345604c36084ae98e80916ca0a109f75864
Bisma-Mithu <bismamithu45@gmail.com>
2026-04-22 20:07:48 +0500

    Basic Concept

---
+++ Basic_concept.py
@@ -0,0 +1,50 @@
+#Swaping
+#1st method
+a=43
+b=39
+none=a
+a=b
+b=none
+print("values")
+print(a,b)
+print(type(a))
+
+#2nd method
+a = 5
+b = 10
+a, b = b, a
+print(a,b)
+
+
+#Casting
+a=23
+u=float(a)
+print(type(u))
+
+b="56"
+t=int(b)
+print(type(t))
+
+c=4.6
+v=str(c)
+print(type(v))
+print(u, t, v)
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
